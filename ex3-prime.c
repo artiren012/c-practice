d8c54f94b1a8b580a47d604e2aadc4718abfee30
Artiren <sketchhydrogenmagnesium@gmail.com>
2023-08-07 19:32:01 +0900

    prime number

---
+++ ex3-prime.c
@@ -0,0 +1,33 @@
+//
+//  ex3-prime.c
+//  c-practice
+//
+
+#include <stdio.h>
+#include <math.h>
+
+#define TRUE 1
+
+int is_prime(int n) {
+    int i, sqrn;
+    sqrn = (int)sqrt(n);
+    for (i = 2; i <= sqrn; i++)
+        if (n % i == 0)
+            return !TRUE;
+    return TRUE;
+}
+
+int main(void) {
+    int n, r, i;
+    while(TRUE) {
+        printf("\nInput number: ");
+        scanf("%d", &n);
+        if (n < 0)
+            continue;
+        if (n == 0)
+            break;
+        r = is_prime(n);
+        printf("%d %s prime number.\n", n, r ? "is" : "is not");
+    }
+    return 0;
+}
